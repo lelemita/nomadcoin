3032d91e3a43c82a596b3090a364515a3d210cd4
lelemita <nywfox@gmail.com>
2021-07-23 19:37:22 +0900

    Refactoring

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,24 +1,61 @@
 package blockchain
 
+import (
+	"crypto/sha256"
+	"fmt"
+	"sync"
+)
+
 type block struct {
-	data string
-	hash string
-	prevHash string
+	Data string
+	Hash string
+	PrevHash string
 }
 
 type blockchain struct {
-	blocks []block
+	// 너무 길어지니까 포인터의 슬라이스로 함
+	blocks []*block
 }
 
 // singleton pattern: only one instance
-var b * blockchain
+var b *blockchain
+// 딱 한번 실행되도록 하기 (goroutin, thread 가 여러개여도..)
+var once sync.Once
+
+func (b *blockchain) AddBlock(data string) {
+	b.blocks = append(b.blocks, createBlock(data))
+}
 
 func GetBlockchain() *blockchain {
 	// 초기화 되었는지 확인하고 딱 한번 생성
 	if b == nil {
-		b = &blockchain{}
+		once.Do(func() {
+			b = &blockchain{}
+			b.AddBlock("Genesis")
+		})
 	}
 	return b
 }
 
+func getLastHash() string {
+	totalBlocks := len(GetBlockchain().blocks)
+	if totalBlocks == 0 {
+		return ""
+	}
+	return GetBlockchain().blocks[totalBlocks - 1].Hash
+}
 
+func (b *block) calculateHash() {
+	hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
+	b.Hash = fmt.Sprintf("%x", hash)
+}
+
+func createBlock(data string) *block {
+	newBlock := block{data, "", getLastHash()}
+	newBlock.calculateHash()
+	return &newBlock
+}
+
+func (b *blockchain) AllBlocks() []*block {
+	return b.blocks
+}
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/lelemita/nomadcoin/blockchain"
+import (
+	"fmt"
+
+	"github.com/lelemita/nomadcoin/blockchain"
+)
 
 func main() {
 	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s \n", block.Data)
+		fmt.Printf("Hash: %s \n", block.Hash)
+		fmt.Printf("PrevHash: %s \n\n", block.PrevHash)
+	}
 }
