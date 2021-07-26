dc58a7759b9ab681a0ec77c0b676e912d0bb3246
lelemita <nywfox@gmail.com>
2021-07-26 16:24:02 +0900

    JSON Decode

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/lelemita/nomadcoin/blockchain"
+	"github.com/lelemita/nomadcoin/utils"
 )
 
 const port string = ":4000"
@@ -16,6 +19,10 @@ type URLDescription struct {
 	Method string `json:"method"`
 	Description string `json:"description"`
 	Payload string `json:"payload,omitempty"`
+}
+
+type AddBlockBody struct {
+	Message string
 }
 
 // type TextMarshaler interface
@@ -35,15 +42,29 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 			URL: URL("/blocks"),
 			Method: "POST",
 			Description: "Add A Block",
-			Payload: "data:string",
+			Payload: "message:string",
 		},
 	}
 	rw.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(rw).Encode(data)
 }
 
+func blocks (rw http.ResponseWriter, r *http.Request) {
+	switch r.Method{
+	case "GET":
+		rw.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
+	case "POST":
+		var addBlockBody AddBlockBody
+		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
+		blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
+		rw.WriteHeader(http.StatusCreated)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", documentation )
+	http.HandleFunc("/blocks", blocks )
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
