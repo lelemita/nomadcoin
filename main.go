1635ab06ed2aabc8c03e865c0a3233ac1f950516
lelemita <nywfox@gmail.com>
2021-08-08 14:14:14 +0900

    Buffered Ch Practice

--- main.go
+++ main.go
@@ -7,28 +7,26 @@ import (
 
 func countToTen(c chan<- int) {
 	for i := range [5]int{} {
-		time.Sleep(500 * time.Millisecond)
 		c <- i
+		fmt.Printf(">> sent : %d <<\n", i)
 	}
 	close(c)
 }
 
 func receive(label string, c <-chan int) {
 	for {
+		time.Sleep(500 * time.Millisecond)
 		num, isOpen := <-c
 		if !isOpen {
 			fmt.Println("We are done.")
 			break
 		}
-		fmt.Println(label, "receive: ", num)
+		fmt.Printf("%s || receive %d ||\n", label, num)
 	}
 }
 
 func main() {
-	c := make(chan int)
+	c := make(chan int, 3)
 	go countToTen(c)
-	go receive("A", c)
-	d := make(chan int)
-	go countToTen(d)
-	receive("BBB", d)
+	receive("A", c)
 }
