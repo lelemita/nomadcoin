06e905baa908b31e575f68b35cb999cc772b76e3
lelemita <nywfox@gmail.com>
2021-08-17 20:57:50 +0900

    openPort

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,6 +3,7 @@ package p2p
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/websocket"
 	"github.com/lelemita/nomadcoin/utils"
@@ -16,11 +17,13 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	}
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
-	initPeer(conn, "xxx", "xxx")
+	openPort := r.URL.Query().Get("openPort")
+	result := strings.Split(r.RemoteAddr, ":")
+	initPeer(conn, result[0], openPort[1:])
 }
 
-func AddPeer(address, port string) {
-	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws", address, port), nil)
+func AddPeer(address, port, openPort string) {
+	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort), nil)
 	utils.HandleErr(err)
 	initPeer(conn, address, port)
 }
