08915e61decc66b0a0be4db1618952b804eb697d
lelemita <nywfox@gmail.com>
2021-08-18 15:13:41 +0900

    Cleanup

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,7 +3,6 @@ package p2p
 import (
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/lelemita/nomadcoin/utils"
@@ -20,16 +19,12 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	}
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
-	peer := initPeer(conn, ip, openPort)
-	time.Sleep(5 * time.Second)
-	peer.inbox <- []byte("Hello! I'm 3000")
+	initPeer(conn, ip, openPort)
 }
 
 func AddPeer(address, port, openPort string) {
 	// Port :4000 is requesting an upgrade from the port :3000
 	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
-	peer := initPeer(conn, address, port)
-	time.Sleep(3 * time.Second)
-	peer.inbox <- []byte("hello from 4000")
+	initPeer(conn, address, port)
 }
--- p2p/peer.go
+++ p2p/peer.go
@@ -9,15 +9,24 @@ import (
 var Peers map[string]*peer = make(map[string]*peer)
 
 type peer struct {
-	conn  *websocket.Conn
-	inbox chan []byte
+	key     string
+	address string
+	port    string
+	conn    *websocket.Conn
+	inbox   chan []byte
+}
+
+func (p *peer) close() {
+	p.conn.Close()
+	delete(Peers, p.key)
 }
 
 func (p *peer) read() {
-	// delete peer in case of error
+	defer p.close()
 	for {
 		_, m, err := p.conn.ReadMessage()
 		if err != nil {
+			fmt.Println(err)
 			break
 		}
 		fmt.Printf("%s", m)
@@ -25,18 +34,25 @@ func (p *peer) read() {
 }
 
 func (p *peer) write() {
+	defer p.close()
 	for {
-		m := <-p.inbox
+		m, ok := <-p.inbox
+		if !ok {
+			break
+		}
 		p.conn.WriteMessage(websocket.TextMessage, m)
 	}
 }
 
 func initPeer(conn *websocket.Conn, address, port string) *peer {
-	p := &peer{
-		conn,
-		make(chan []byte),
-	}
 	key := fmt.Sprintf("%s:%s", address, port)
+	p := &peer{
+		key:     key,
+		address: address,
+		port:    port,
+		conn:    conn,
+		inbox:   make(chan []byte),
+	}
 	Peers[key] = p
 	go p.read()
 	go p.write()
