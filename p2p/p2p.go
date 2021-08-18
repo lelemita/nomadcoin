287e43b19e877dafb23d2fd6748b3a2b8c23c839
lelemita <nywfox@gmail.com>
2021-08-18 13:40:51 +0900

    Inbox

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,6 +3,7 @@ package p2p
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/lelemita/nomadcoin/utils"
@@ -19,14 +20,16 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	}
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
-	initPeer(conn, ip, openPort)
-	conn.WriteMessage(websocket.TextMessage, []byte("Hello 4000!!"))
+	peer := initPeer(conn, ip, openPort)
+	time.Sleep(5 * time.Second)
+	peer.inbox <- []byte("Hello! I'm 3000")
 }
 
 func AddPeer(address, port, openPort string) {
 	// Port :4000 is requesting an upgrade from the port :3000
 	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
-	initPeer(conn, address, port)
-	conn.WriteMessage(websocket.TextMessage, []byte("Hello 3000, I'm 4000"))
+	peer := initPeer(conn, address, port)
+	time.Sleep(3 * time.Second)
+	peer.inbox <- []byte("hello from 4000")
 }
