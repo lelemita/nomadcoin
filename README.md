d2eedbc2aa1dacdd3ccb4c9d4f9f17043ea2ec33
lelemita <nywfox@gmail.com>
2021-08-23 21:16:23 +0900

    Table Tests

--- README.md
+++ README.md
@@ -23,7 +23,6 @@ go run main.go -mode=rest -port=4000
 ```bash
 go test ./... -v
 go test -v -coverprofile cover.out  ./...
-# 테스트 하는 비율 % 표시 됨
 go tool cover -html='cover.out'
-# 녹색은 테스트 된거, 적색은 안된거
+# 녹색은 테스트 통과한 거, 적색은 안된거
 ```
--- utils/utils_test.go
+++ utils/utils_test.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"reflect"
 	"testing"
@@ -48,5 +49,44 @@ func TestToBytes(t *testing.T) {
 		if b != bys[i] {
 			t.Errorf("Expected %d, got %d", bys[i], b)
 		}
+	}
+}
+
+// table test
+func TestSplitter(t *testing.T) {
+	type test struct {
+		input  string
+		sep    string
+		index  int
+		output string
+	}
+	tests := []test{
+		{"0:5:0", ":", 1, "5"},
+		{"0:5:0", ":", 10, ""},
+		{"0:5:0", "-", 0, "0:5:0"},
+	}
+	for _, tc := range tests {
+		got := Splitter(tc.input, tc.sep, tc.index)
+		if got != tc.output {
+			t.Errorf("Expected %s and got %s", tc.output, got)
+		}
+	}
+}
+
+// 콘솔 출력, 로그 출력을 테스트 하는 방법.
+// 출력 부분을 별도 함수로 빼고, 그 함수를 체크할 수 있는 함수로 잠시 바꿔서 테스트함
+func TestHandleErr(t *testing.T) {
+	oldLogFn := logFn
+	defer func() {
+		logFn = oldLogFn
+	}()
+	called := false
+	logFn = func(v ...interface{}) {
+		called = true
+	}
+	err := errors.New("test")
+	HandleErr(err)
+	if !called {
+		t.Error("HandleError should call fn")
 	}
 }
