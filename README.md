b0983e8d39a9336e1ca85315961aa6dbac3e354c
lelemita <nywfox@gmail.com>
2021-08-23 20:26:45 +0900

    Test Coverage

--- README.md
+++ README.md
@@ -13,3 +13,17 @@ https://nomadcoders.co/nomadcoin/lobby
 * HTML Explorer
 * P2P (Websockets)
 * Unit Testing
+
+### Run example
+```bash
+go run main.go -mode=rest -port=4000
+```
+
+### Test
+```bash
+go test ./... -v
+go test -v -coverprofile cover.out  ./...
+# 테스트 하는 비율 % 표시 됨
+go tool cover -html='cover.out'
+# 녹색은 테스트 된거, 적색은 안된거
+```
--- utils/utils_test.go
+++ utils/utils_test.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"encoding/hex"
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -32,4 +33,20 @@ func ExampleHash() {
 	x := Hash(s)
 	fmt.Println(x)
 	// Output: e005c1d727f7776a57a661d61a182816d8953c0432780beeae35e337830b1746
+}
+
+func TestToBytes(t *testing.T) {
+	bys := []byte{7, 12, 0, 4, 116, 101, 115, 116}
+	s := "test"
+	bs := ToBytes(s)
+	// type check
+	kind := reflect.TypeOf(bs).Kind()
+	if kind != reflect.Slice {
+		t.Errorf("ToBytes should return a slice of bytes got %s\n", kind)
+	}
+	for i, b := range bs {
+		if b != bys[i] {
+			t.Errorf("Expected %d, got %d", bys[i], b)
+		}
+	}
 }
