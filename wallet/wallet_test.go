992255a486cc54e4db6c781c0b9e1608930ffd92
lelemita <nywfox@gmail.com>
2021-08-24 18:35:48 +0900

    Test verify

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -9,9 +9,10 @@ import (
 const (
 	// //toGetTestKey() 참조
 	testKey string = "30770201010420722f026193ca462a71ef1bbca650f4c93e2920297310fef8ab03656934473a87a00a06082a8648ce3d030107a144034200041ea0b7a88eb16ecce8cac6e99c1ca7fbe87fa909708cbea6f591690b5efa1a41dadfea9638baf06343918fc521ec5513771bea492a81036672e9c570e156b4e6"
+	testSig string = "12232ff554b277720ad5fe38a5785e1872a6b4523814a71ef4ade3170647af3e1e3b59a796a970bc7cb46971cda372a6685b82c134391121c3316bd6dea26d67"
 	// 유효한 hexadecimal 아무거나 (이거는 block hash 가져옴)
-	testPayload string = "0001961555cb88268649070d839cc7bb8951f4512ad1faa024cc6dd19bafb193"
-	testSig     string = "12232ff554b277720ad5fe38a5785e1872a6b4523814a71ef4ade3170647af3e1e3b59a796a970bc7cb46971cda372a6685b82c134391121c3316bd6dea26d67"
+	testPayload  string = "0001961555cb88268649070d839cc7bb8951f4512ad1faa024cc6dd19bafb193"
+	wrongPayload string = "c2bb8314fa7640b970a2a5ca49b529c4d09129d949119b1bcb1388dd9ad419dc"
 )
 
 func makeTestWallet() *wallet {
@@ -26,6 +27,31 @@ func TestSign(t *testing.T) {
 	_, err := hex.DecodeString(sig)
 	if err != nil {
 		t.Errorf("Sign() should return a hex encoded string, got %s\n", sig)
+	}
+}
+
+func TestVerify(t *testing.T) {
+	type test struct {
+		ok    bool
+		input string
+	}
+	tests := []test{
+		{true, testPayload},
+		{false, wrongPayload},
+	}
+	for _, tc := range tests {
+		w := makeTestWallet()
+		ok := Verify(testSig, tc.input, w.Address)
+		if ok != tc.ok {
+			t.Error("Verify() could not verify testSignature and testPayload")
+		}
+	}
+}
+
+func TestRestoreBigInts(t *testing.T) {
+	_, _, err := restoreBigInts("xxxxxxx")
+	if err == nil {
+		t.Error("restoreBigInts should return error when payload is not hex")
 	}
 }
 
